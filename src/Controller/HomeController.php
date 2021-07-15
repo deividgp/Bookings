e26cad4857a17e7efe3423034fb5eb9d981feaa1
deividgp <dayd4@outlook.com>
2021-07-15 13:25:36 +0200

    Featured trip added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TripRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,14 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(TripRepository $tripRepository): Response
     {
         $user = $this->getUser();
 
         if($user){
             $this->get('session')->set('admin', false);
+            $trips = $tripRepository->findAll();
+            $random = rand(0, sizeof($trips)-1);
+
             return $this->render('home/index.html.twig', [
-                'controller_name' => 'HomeController',
+                'trip' => $trips[$random],
             ]);
         }else{
             return $this->redirectToRoute('app_login');
